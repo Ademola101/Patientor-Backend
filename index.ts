258429bebe7388b16c5b5a921656581a5011d772
Ademola <ademolaogunmokun@yahoo.com>
2022-09-04 21:48:40 +0100

    change name

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-import express from 'express'
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const PORT = 3002;
+app.get('/ping', (_req, res) => {
+  console.log('someone pinged me');
+  res.send('Hello world');
+  
+});
+
+app.listen(PORT, () => {
+  console.log('server running');
+  
+});
