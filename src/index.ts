34253562675dea46053c26b219c8d1901eb53f45
Ademola <ademolaogunmokun@yahoo.com>
2022-09-05 02:06:47 +0100

    set to render the api to the frontend

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import express from 'express';
 import diagnosisRouter from './router/diagnosis';
 import patientsRouter from './router/Patient';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
+// dowload and use the typed version
 app.use(express.json());
 app.use('/api/diagnosis', diagnosisRouter);
 app.use('/api/patients', patientsRouter);
 
 
 
-const PORT = 3002;
+const PORT = 3001;
 app.get('/ping', (_req, res) => {
   console.log('someone pinged me');
   res.send('Hello world');
